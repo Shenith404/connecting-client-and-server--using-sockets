933a4973ed4cf2af388c0040ef1d25f7ca55c1f2
Shenith <shenithbandara@gmail.com>
2024-03-29 16:11:32 +0530

    chat

--- client_server/src/client_server/EchoClient.java
+++ client_server/src/client_server/EchoClient.java
@@ -1,6 +1,8 @@
 package client_server;
 
+import java.io.*; //can import all packages
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class EchoClient {
@@ -10,7 +12,19 @@ public class EchoClient {
 		System.out.println("This is Client");
 		//create socket ,in here we use out ip address
 		Socket soc =new Socket("localhost",9806);
-
+		
+		//read the user input from keybord
+		BufferedReader  userInput = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter the string ");
+		
+		String str = userInput.readLine();//get the user input and 
+		
+		PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+		out.println(str);
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+		System.out.println(in.readLine());
 	}
 
 }
--- client_server/src/client_server/EchoServer.java
+++ client_server/src/client_server/EchoServer.java
@@ -1,6 +1,7 @@
 package client_server;
 
-import java.io.IOException;
+import java.io.*; //can import all packages
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,8 +14,22 @@ public class EchoServer {
 		ServerSocket ss =new ServerSocket(9806);
 		//waiting for incoming client connection
 		Socket soc =ss.accept();
+		
+		
 		System.out.println("Connection estabilshed");
+		BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+		
+		String str = in.readLine();
+		System.out.println("Client says : "+str);
 
+		
+		BufferedReader  userInput = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter the string ");
+		
+		String reply = userInput.readLine();
+		PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+		out.println("server says : "+ reply);
 	}
 
 }
